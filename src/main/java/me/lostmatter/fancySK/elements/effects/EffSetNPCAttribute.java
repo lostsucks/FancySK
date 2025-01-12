f1148b372d7f9dbeb7fd31e6e87d01b7f9c1e95c
Elisha <lostmatterlmao@gmail.com>
2025-01-12 01:19:42 -0600

    Added setting attribute syntax (ONLY PLAYERENTITIES WORK)

---
+++ src/main/java/me/lostmatter/fancySK/elements/effects/EffSetNPCAttribute.java
@@ -0,0 +1,57 @@
+package me.lostmatter.fancySK.elements.effects;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.lang.Effect;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.SkriptParser;
+import ch.njol.util.Kleenean;
+import de.oliver.fancynpcs.api.FancyNpcsPlugin;
+import de.oliver.fancynpcs.api.Npc;
+import de.oliver.fancynpcs.api.NpcAttribute;
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.Event;
+import org.jetbrains.annotations.NotNull;
+
+public class EffSetNPCAttribute extends Effect {
+
+    static {
+        Skript.registerEffect(EffSetNPCAttribute.class,
+                "set [the] npc attribute %string% of %string% to %string%"
+        );
+    }
+
+    private Expression<String> attributeExpression;
+    private Expression<String> targetNpcExpression;
+    private Expression<String> valueExpression;
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public boolean init(Expression<?>[] exprs, int matchedPattern, @NotNull Kleenean isDelayed, @NotNull SkriptParser.ParseResult parseResult) {
+        attributeExpression = (Expression<String>) exprs[0];
+        targetNpcExpression = (Expression<String>) exprs[1];
+        valueExpression = (Expression<String>) exprs[2];
+        return true;
+    }
+
+    @Override
+    protected void execute(@NotNull Event event) {
+        Npc npc = FancyNpcsPlugin.get().getNpcManager().getNpc(targetNpcExpression.getSingle(event));
+        if (npc == null) return;
+
+        if (npc.getData().getType() != EntityType.PLAYER) {
+            Skript.error("The npc must be a player npc to set attributes");
+            return;
+        }
+
+        NpcAttribute attribute = FancyNpcsPlugin.get().getAttributeManager().getAttributeByName(EntityType.PLAYER, attributeExpression.getSingle(event));
+        npc.getData().addAttribute(attribute, valueExpression.getSingle(event));
+
+        npc.updateForAll();
+    }
+
+    @Override
+    public @NotNull String toString(@NotNull Event event, boolean b) {
+        return "set " + attributeExpression.toString(event, b) + " of " + targetNpcExpression.toString(event, b) + " to " + valueExpression.toString(event, b);
+    }
+
+}
